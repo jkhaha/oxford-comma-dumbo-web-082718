7ca1408ba86eb3f997b4e3510439766cff3df029
Jackie Ha <jha2@wellesley.edu>
2018-08-20 20:24:34 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,5 +1,9 @@
 def oxford_comma(array)
   if array.size == 1 
-    return array[0]
-  array.join(",")
+    return array.join
+  elseif array.size == 2 
+    return array.join(" and ")
+  else
+    return array[0..-1].join(",")
+  end 
 end
